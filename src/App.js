953da93e1ff1a9b5c2a89f261cb7a4a0d65d31ce
SamchanThunder <samchan.lee@gmail.com>
2024-06-21 16:07:34 +0900

    Update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import { RouteFunction } from './scripts/routes';
 import { Header } from './pages/header';
 import { AuthDetails } from './scripts/authdetails';
+import {isMobile} from 'react-device-detect';
 import './App.css';
 
 function App() {
-
+  if(isMobile){
+    alert("This game is meant to be played on the computer. Turn your phone horizontal for best gameplay.")
+  }
   return (
     <div>
       <RouteFunction />
