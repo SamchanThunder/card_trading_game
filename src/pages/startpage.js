a3310501f81fbef269f39a5a12e4905d7af6d41d
SamchanThunder <samchan.lee@gmail.com>
2024-06-27 00:41:38 +0900

    Update startpage.js

--- src/pages/startpage.js
+++ src/pages/startpage.js
@@ -6,7 +6,7 @@ export function StartPage() {
             <div id="titleStart"><br/ ><u>Game Functions (READ ME)</u></div>
             <br />
             <div id="bodyStart">
-                <b>Note:</b> This projected was completed in a week. There may be bugs.
+                <b>Note:</b> This project was completed in a week. There may be bugs.
             </div>
             <div id="bodySecond">
                 <br />
@@ -15,7 +15,7 @@ export function StartPage() {
                 2. Get cards by buying packs.<br />
                 3. View your inventory and sell unwanted cards by clicking on them.<br />
                 4. Trade your cards for other cards or won. <br />
-                5. Everyday, a random mythic card appears in the auction (500k to 1m won)<br />
+                5. Everyday (8PM EST), a random mythic card appears in the auction (500k to 1m won)<br />
                 <br />
                 <div id="packChance"><u>Pack Chances:</u> Common Pack(70% common, 25% rare, 5% epic); Rare Pack(15% common 70% rare 15% epic); Epic Pack(20% rare, 70% epic 10% legendary); Legendary Pack(20% epic, 80% legendary)</div>
             </div>
